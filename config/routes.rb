70b4ed28a05a5880542be8bbb524cd819fc91ffb
Meshack Korir <meshackkorir100@gmail.com>
2023-06-23 13:35:59 +0300

    created custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :birds, only: [:index, :show, :create, :update]
+  # creating custom routes
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
