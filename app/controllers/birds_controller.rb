1b058225e281c4e8907c048eb57245220dcc1ec8
Meshack Korir <meshackkorir100@gmail.com>
2023-06-23 13:33:51 +0300

    updated like

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -20,21 +20,21 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
 
-    def update
-      bird = Bird.find_by(id: params[:id])
-      if bird
-        bird.update(bird_params)
-        render json: bird
-      else
-        render json: { error: "Bird not found" }, status: :not_found
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
     end
   end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
